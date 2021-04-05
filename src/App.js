927c35855c1f7905f7856b60a0210ff82b6e47ec
shubham0514 <55954727+shubham0514@users.noreply.github.com>
2021-04-05 11:53:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+const items = [
+  {
+    title:'What is React?',
+    content:'React is a front end javascript framework',
+  },
+  {
+    title:'Why use React?',
+    content:'React is favourite js library',
 
-function App() {
+  },
+  {
+    title:'what is special about React',
+    content:'React is component based ',
+
+  },
+]
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue',
+  },
+];
+
+
+export default () => {
+  
+  const [selected ,setSelected] = useState(options[0]);
+ /* const[showDropdown,setShowDropdown] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button onClick ={()=>setShowDropdown(!showDropdown)} >Toggle Dropdown</button>
+      {showDropdown?
+      <Dropdown
+       options={options}
+       selected={selected}
+       onSelectedChange={setSelected}
+       /> :null}
     </div>
   );
-}
-
-export default App;
+  */
+  return(
+    <div>
+      <Header />
+   <Route path='/'>
+    <Accordion items={items}/>
+   </Route>
+   <Route path='/list'>
+     <Search/>
+   </Route>
+    <Route path='/dropdown'>
+      <Dropdown 
+      label='Select a Color'
+      options={options}
+      selected={selected}
+      onSelectedChange={setSelected}
+      />
+    </Route>
+    <Route path='/translate'>
+      <Translate/>
+    </Route>
+    </div>
+  );
+};
